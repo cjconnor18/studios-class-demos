df50f86ef57b9035c192f07ea2994f23d4504463
cjallen1988 <cjallen1988@gmail.com>
2021-09-14 20:59:51 -0500

    studio 3

---
+++ src/com/company/studios/studio3/Main.java
@@ -0,0 +1,11 @@
+package com.company.studios.studio3;
+
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Date newDate = new Date();
+        System.out.println(newDate.toString());
+    }
+}
