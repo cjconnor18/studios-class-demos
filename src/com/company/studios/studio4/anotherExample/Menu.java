f501ddbc047e3cb78a9932a4ec1b3a8b674dfe18
cjallen1988 <cjallen1988@gmail.com>
2021-09-19 15:32:36 -0500

    studio 4 another example added

--- src/com/company/studios/studio4/anotherExample/Menu.java
+++ src/com/company/studios/studio4/anotherExample/Menu.java
@@ -7,7 +7,7 @@ public class Menu {
     private ArrayList<MenuItem> menuItems;
     private Date lastUpdated;
 
-    //Constructors
+    //Constructors, Overloaded, empty
     public Menu(ArrayList<MenuItem> aMenuItems, Date lastUpdated) {
         this.menuItems = aMenuItems;
         this.lastUpdated = lastUpdated;
@@ -17,7 +17,66 @@ public class Menu {
         this(aMenuItems, new Date());
     }
 
-    public Menu() {}
+    public Menu() {
+        this(new ArrayList<MenuItem>());
+    }
+
+    //methods
+    public void printMenu() {
+        ArrayList<MenuItem> appetizers = new ArrayList<>();
+        ArrayList<MenuItem> main = new ArrayList<>();
+        ArrayList<MenuItem> desserts = new ArrayList<>();
+
+        for(MenuItem item : menuItems) {
+            if(item.getCategory().equals("appetizer")) {
+                appetizers.add(item);
+            } else if(item.getCategory().equals("main")) {
+                main.add(item);
+            } else if(item.getCategory().equals("dessert")) {
+                desserts.add(item);
+            }
+        }
+
+        System.out.println("\nRestaurant Menu (Last Updated: " + this.lastUpdated + ")");
+        System.out.println("\tAppetizers");
+        MenuItem.printHeading();
+        printCategory(appetizers);
+
+        System.out.println("\n\tMain Course");
+        MenuItem.printHeading();
+        printCategory(main);
+
+        System.out.println("\n\tDesserts");
+        MenuItem.printHeading();
+        printCategory(desserts);
+
+    }
+
+    private void printCategory(ArrayList<MenuItem> items) {
+        if(items.size() > 0) {
+            for(MenuItem item : items) {
+                item.printOne();
+            }
+        } else {
+            System.out.println("Nothing listed. Check back later!");
+        }
+    }
+
+    public void addItem(MenuItem item) {
+        menuItems.add(item);
+        updateDateLastUpdated();
+    }
+
+    public void removeItem(MenuItem item) {
+        if(menuItems.contains(item)) {
+            menuItems.remove(item);
+            updateDateLastUpdated();
+        }
+    }
+
+    private void updateDateLastUpdated() {
+        this.lastUpdated = new Date();
+    }
 
     //Getters and Setters
     public ArrayList<MenuItem> getMenuItems() {
@@ -32,7 +91,10 @@ public class Menu {
         return lastUpdated;
     }
 
+    /*
+don't really need a setter for lastUpdated; anytime updates are made to menu, we have a private method to do this.
     public void setLastUpdated(Date lastUpdated) {
         this.lastUpdated = lastUpdated;
     }
+*/
 }
--- src/com/company/studios/studio4/anotherExample/Restaurant.java
+++ src/com/company/studios/studio4/anotherExample/Restaurant.java
@@ -2,9 +2,44 @@ package com.company.studios.studio4.anotherExample;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 public class Restaurant {
+    public static void main(String[] args) throws InterruptedException {
+        MenuItem steak = new MenuItem(12.99, "Ribeye", "main", false);
+        MenuItem steak2 = new MenuItem(15.99, "T-Bone", "main", false);
+        MenuItem steak3 = new MenuItem(17.99, "KC Strip", "main", false);
+        MenuItem iceCream = new MenuItem(2.99, "Strawberry Ice Cream", "dessert", false);
+        MenuItem iceCream2 = new MenuItem(2.99, "Vanilla Ice Cream", "dessert");
+        MenuItem iceCream3 = new MenuItem(2.99, "Chocolate Ice Cream", "dessert", false);
+        MenuItem app = new MenuItem(5.99, "Fried Pickles", "appetizer");
+        MenuItem app2 = new MenuItem(4.99, "Queso and Chips", "appetizer", false);
+        MenuItem app3 = new MenuItem(5.99, "Salad", "appetizer", false);
 
+        Menu menu = new Menu();
+
+        menu.addItem(steak);
+        menu.addItem(steak2);
+        menu.addItem(steak3);
+        menu.addItem(iceCream);
+        menu.addItem(iceCream2);
+        menu.addItem(iceCream3);
+        menu.addItem(app);
+        menu.addItem(app2);
+        menu.addItem(app3);
+
+        menu.printMenu();
+
+
+
+// the next line is just adding a delay so we can look at time signature in lastUpdated
+        TimeUnit.SECONDS.sleep(50);
+        menu.removeItem(app2);
+        menu.removeItem(steak2);
+
+        menu.printMenu();
+
+    }
 
 
 }
